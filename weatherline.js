39a0fc6b7d2a6c9fbbe12ae7e53cfa5cb33afd69
James Ramsay <james@jramsay.com.au>
2022-07-17 12:42:23 +1000

    Partially fix screen size to widget size mapping

--- weatherline.js
+++ weatherline.js
@@ -1,10 +1,12 @@
 /**
  * Scriptable Weather Line widget
+ * <https://github.com/jamesramsay/weatherline>
+ * By James Ramsay.
  */
 
 // Development configuration
 const debugParams = {
-  // locationGeohash: 'r1r14c',
+  locationGeohash: 'r1r14c',
   screenSize: new Size(414, 896),
   debugBorders: false,
 };
@@ -489,34 +491,28 @@ function getWidgetSizeInPoints(widgetFamily, screenSize) {
   // stringify device screen size
   const devSize = (({width: w, height: h}) => w > h ? `${h}x${w}` : `${w}x${h}`)(screenSize);
 
-  // screen size to widget size mapping for iPhone, excluding the latest iPhone 12 series. iPad size
+  // Source: https://developer.apple.com/design/human-interface-guidelines/components/system-experiences/widgets/#specifications
   const sizeMap = {
-    // iPad Mini 2/3/4, iPad 3/4, iPad Air 1/2. 9.7" iPad Pro
-    // '768x1024': { small: [0, 0], medium: [0, 0], large: [0, 0] },
-    // 10.2" iPad
-    // '810x1080': { small: [0, 0], medium: [0, 0], large: [0, 0] },
-    // 10.5" iPad Pro, 10.5" iPad Air 3rd Gen
-    // '834x1112': { small: [0, 0], medium: [0, 0], large: [0, 0] },
-    // 10.9" iPad Air 4th Gen
-    // '820x1180': { small: [0, 0], medium: [0, 0], large: [0, 0] },
-    // 11" iPad Pro
-    '834x1194': { small: [155, 155], medium: [329, 155], large: [345, 329] },
-    // 12.9" iPad Pro
-    '1024x1366': { small: [170, 170], medium: [332, 170], large: [382, 332] },
-    // 12 Pro Max
-    // '428x926': { small: [0, 0], medium: [0, 0], large: [0, 0] },
-    // XR, 11, 11 Pro Max
+    // iOS
+    '428x926': { small: [170, 170], medium: [364, 170], large: [364, 382] },
     '414x896': { small: [169, 169], medium: [360, 169], large: [360, 376] },
-    // 12, 12 Pro
-    // '390x844': { small: [0, 0], medium: [0, 0], large: [0, 0] },
-    // X, XS, 11 Pro, 12 Mini
-    '375x812': { small: [155, 155], medium: [329, 155], large: [329, 345] },
-    // 6/7/8(S) Plus
     '414x736': { small: [159, 159], medium: [348, 159], large: [348, 357] },
-    // 6/7/8(S) and 2nd Gen SE
+    '390x844': { small: [158, 158], medium: [338, 158], large: [338, 354] },
+    '375x812': { small: [155, 155], medium: [329, 155], large: [329, 345] },
     '375x667': { small: [148, 148], medium: [322, 148], large: [322, 324] },
-    // 1st Gen SE
-    '320x568': { small: [141, 141], medium: [291, 141], large: [291, 299] }
+    '360x780': { small: [155, 155], medium: [329, 155], large: [329, 345] },
+    '320x568': { small: [141, 141], medium: [292, 141], large: [292, 311] },
+    // iPadOS
+    // '768x1024': { small: [], medium: [], large: [], extraLarge: [] },
+    // '744x1133': { small: [], medium: [], large: [], extraLarge: [] },
+    // '810x1080': { small: [], medium: [], large: [], extraLarge: [] },
+    // '820x1180': { small: [], medium: [], large: [], extraLarge: [] },
+    // '834x1112': { small: [], medium: [], large: [], extraLarge: [] },
+    '834x1194': { small: [136, 136], medium: [300, 136], large: [300, 300], extraLarge: [628, 300] },
+    // '954x1373': { small: [], medium: [], large: [], extraLarge: [] },
+    // '970x1389': { small: [], medium: [], large: [], extraLarge: [] },
+    '1024x1366': { small: [160, 160], medium: [356, 160], large: [356, 356], extraLarge: [748, 356] },
+    // '1192x1590': { small: [], medium: [], large: [], extraLarge: [] },
   }
   let widgetSizeInPoint = null
 
@@ -737,7 +733,6 @@ async function getLocationGeohash() {
  * @param   {number} [precision] - Number of characters in resulting geohash.
  * @returns {string} Geohash of supplied latitude/longitude.
  * @throws  Invalid geohash.
- *
  * @example
  *     var geohash = Geohash.encode(52.205, 0.119, 7); // geohash: 'u120fxw'
  */
@@ -793,6 +788,27 @@ function encodeGeohash(lat, lon, precision = 6) {
 
   return geohash;
 }
+
+/**
+ * Forecast interface permits multiple forecaster implementations with differing data structures
+ */
+
+/**
+ * Bureau of Meteorology (BOM) forecaster implementation
+ */
+const Forecast = {
+  _geohash: '',
+  locationName: '',
+  fetchedAt: '', // The time when the forecast was last fetched from the forecaster.
+  updatedAt: '', // The time when the forecast was last updated by the forecaster.
+  hourly: [],
+  daily: [],
+}
+
+Forecast.fetch = () => { return 'test' };
+
+
+
 
 module.exports = {
   main,
